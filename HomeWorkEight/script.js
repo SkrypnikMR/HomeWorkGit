d7e78da754c4619782ccb5d116fe4864396a0846
SkripnikProgram <skripnikmrw@gmail.com>
2021-04-10 00:26:12 +0300

    change cache function forideal option

--- HomeWorkEight/script.js
+++ HomeWorkEight/script.js
@@ -166,13 +166,15 @@ function cache(func) {
   var cache = [];
 
   return function (arg1, arg2) {
+
     for (var i = 0; i < cache.length; i++) {
       if (arguments[0] === cache[i].arg1 && arguments[1] === cache[i].arg2) {
-        return cache[i].arg1 + cache[i].arg2;
+        return `Это есть в cache, держи ${cache[i].result}`
       }
     }
-    cache.push({ arg1: arguments[0], arg2: arguments[1] });
-    return func(arg1, arg2);
+    var result = func(arg1, arg2);
+    cache.push({ arg1: arguments[0], arg2: arguments[1], result});
+    return result;
   };
 }
 
