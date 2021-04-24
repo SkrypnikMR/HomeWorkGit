06fc4c78c66266d808c36f381f33eec6cdaa8c5c
SkripnikProgram <skripnikmrw@gmail.com>
2021-04-24 23:05:31 +0300

    more tests

--- HomeWorkDataStructure/DataStructure/Tests/ArrayList.test.js
+++ HomeWorkDataStructure/DataStructure/Tests/ArrayList.test.js
@@ -223,11 +223,211 @@ describe("ArrayList toString", function () {
     var a = new ArrayList();
     expect(typeof a.toString).toBe("function");
   });
-  it("should called ", function () {
+  it("should called with 1 element in collection", function () {
     var a = new ArrayList(3);
     var testValue = 10;
     a.add(testValue);
     var result = a.toString();
-    expect(result).toEqual('10 ');
+    expect(result).toEqual("10");
+  });
+  it("should called with 3 element in collection", function () {
+    var a = new ArrayList(3);
+    var testValue = 10;
+    a.add(testValue);
+    a.add(testValue);
+    a.add(testValue);
+    var result = a.toString();
+    expect(result).toEqual("10,10,10");
+  });
+});
+describe("ArrayList remove", function () {
+  var test = new ArrayList(3);
+  it("should be defined ", function () {
+    expect(test.remove).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof test.remove).toBe("function");
+  });
+  it("should value was delete from collection", function () {
+    var a = new ArrayList(3);
+    var testArray = [undefined, 2, undefined];
+    a.add(1);
+    a.add(2);
+    a.remove(1);
+    expect(a.array).toEqual(testArray);
+  });
+  it("should return a value after delete", function () {
+    var b = new ArrayList(3);
+    b.add(1);
+    b.add(2);
+    expect(b.remove(1)).toBe(1);
+  });
+  it("should change size to size--", function () {
+    var a = new ArrayList(3);
+    a.add(1);
+    a.add(2);
+    var oldSize = a.getSize();
+    a.remove(1);
+    expect(a.getSize()).toEqual(oldSize - 1);
+  });
+  it("should catch error with true error message", function () {
+    try {
+      var a = new ArrayList(3);
+      a.add(1);
+      a.add(2);
+      a.remove(3);
+    } catch (e) {
+      expect(e.message).toBe(`don't have this value in collection`);
+    }
+  });
+});
+describe("ArrayList contains", function () {
+  var test = new ArrayList(3);
+  test.add(2);
+  it("should be defined ", function () {
+    expect(test.remove).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof test.remove).toBe("function");
+  });
+  it("should return true", function () {
+    expect(test.contains(2)).toBe(true);
+  });
+  it("should return false", function () {
+    expect(test.contains(8)).toBe(false);
+  });
+});
+describe("ArrayList minValue", function () {
+  var test = new ArrayList(3);
+  test.add(2);
+  test.add(23);
+  test.add(33);
+  test.add(4);
+  test.add(1);
+  it("should be defined ", function () {
+    expect(test.minValue).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof test.minValue).toBe("function");
+  });
+  it("should return minimal value of collection", function () {
+    expect(test.minValue()).toBe(1);
+  });
+  it("should throw error collection is empty", function () {
+   try{
+    var sad = new ArrayList();
+    sad.minValue();
+   }catch(e){
+    expect(e.message).toBe('collection is empty');
+   }
+  
+  });
+});
+describe("ArrayList maxValue", function () {
+  var test = new ArrayList(3);
+  test.add(2);
+  test.add(23);
+  test.add(33);
+  test.add(4);
+  test.add(1);
+  it("should be defined ", function () {
+    expect(test.maxValue).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof test.maxValue).toBe("function");
+  });
+  it("should return maximal value of collection", function () {
+    expect(test.maxValue()).toBe(33);
+  });
+  it("should throw error collection is empty", function () {
+   try{
+    var sad = new ArrayList();
+    sad.maxValue();
+   }catch(e){
+    expect(e.message).toBe('collection is empty');
+   }
+  
+  });
+});
+describe("ArrayList minIndex", function () {
+  var test = new ArrayList(3);
+  test.add(2);
+  test.add(23);
+  test.add(33);
+  test.add(4);
+  test.add(1);
+  it("should be defined ", function () {
+    expect(test.minIndex).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof test.minIndex).toBe("function");
+  });
+  it("should return minimal value of collection", function () {
+    expect(test.minIndex()).toBe(4);
+  });
+  it("should throw error collection is empty", function () {
+   try{
+    var sad = new ArrayList();
+    sad.minIndex();
+   }catch(e){
+    expect(e.message).toBe('collection is empty');
+   }
+  
+  });
+});
+describe("ArrayList maxIndex", function () {
+  var test = new ArrayList(3);
+  test.add(2);
+  test.add(23);
+  test.add(33);
+  test.add(4);
+  test.add(1);
+  it("should be defined ", function () {
+    expect(test.maxIndex).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof test.maxIndex).toBe("function");
+  });
+  it("should return minimal value of collection", function () {
+    expect(test.maxIndex()).toBe(2);
+  });
+  it("should throw error collection is empty", function () {
+   try{
+    var sad = new ArrayList();
+    sad.maxIndex();
+   }catch(e){
+    expect(e.message).toBe('collection is empty');
+   }
+  
+  });
+});
+describe("ArrayList reverse", function () {
+  var test = new ArrayList(5);
+  test.add(1);
+  test.add(3);
+  it("should be defined ", function () {
+    expect(test.reverse).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof test.reverse).toBe("function");
+  });
+  it("should throw error collection is empty", function () {
+   try{
+     var sad = new ArrayList(3);
+    sad.reverse();
+   }catch(e){
+    expect(e.message).toBe('collection is empty');
+   }
+  });
+  it("should collection was reversed with empty slots", function () {
+    var testArray = [3,1,undefined,undefined,undefined];
+    test.reverse();
+    expect(test.array).toEqual(testArray);
+  });
+  it("should collection was reversed", function () {
+    var testik = new ArrayList([2,3,4]);
+    var testArray = [4,3,2];
+    testik.reverse();
+    expect(testik.array).toEqual(testArray);
   });
 });
