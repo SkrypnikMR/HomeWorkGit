0f78ffdfee2031a2bd08711659e4a2d88428ecff
SkripnikProgram <skripnikmrw@gmail.com>
2021-05-05 01:22:03 +0300

    more functionality of my Binary Tree

--- HomeWorkDataStructure/DataStructure/Tests/BinaryTree.test.js
+++ HomeWorkDataStructure/DataStructure/Tests/BinaryTree.test.js
@@ -145,14 +145,193 @@ describe("BinaryTree search", function () {
   it("should return -1 if serchValue === 0", function () {
     var bTreeTest1 = new BinaryTree();
     var testSearchValue = 4;
-    bTreeTest1.init([1,2,3]);
+    bTreeTest1.init([1, 2, 3]);
     expect(bTreeTest1.search(testSearchValue)).toBe(-1);
   });
   it("should return node if searchValue === argument value ", function () {
     var bTreeTest2 = new BinaryTree();
     var testSearchValue = 4;
-    bTreeTest2.init([1,2,3,24,345,5,-100,3,4]);
-    console.log(bTreeTest2.toArray());
-    expect(bTreeTest2.search(testSearchValue)).toEqual({value:4, left:null, right:null});
+    bTreeTest2.init([1, 2, 3, 24, 345, 5, -100, 3, 4]);
+    expect(bTreeTest2.search(testSearchValue)).toEqual({
+      value: 4,
+      left: null,
+      right: null,
+    });
+  });
+});
+describe("BinaryTree remove", function () {
+  var bTree = new BinaryTree();
+  it("should be defined ", function () {
+    expect(bTree.remove).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof bTree.remove).toBe("function");
+  });
+  it("this search return -1 ", function () {
+    var test1BT = new BinaryTree();
+    var value = 4;
+    test1BT.init([1, 2, 3]);
+    var mockSearch = jest.fn().mockReturnValue(-1);
+    test1BT.search = mockSearch;
+    expect(test1BT.remove(value)).toBe(-1);
+    expect(mockSearch).toHaveBeenCalledWith(value);
+  });
+  it("should remove value and remove this value from secretArray ", function () {
+    var test2BT = new BinaryTree();
+    var value = 38;
+    var testArray = [30, 18, 22, 31, 34, 21, 15];
+    test2BT.init([30, 18, 22, 38, 31, 34, 21, 15]);
+    test2BT.remove(value);
+    expect(test2BT.secretArray).toEqual(testArray);
+  });
+  it("should remove value and remove this value from secretArray and value in secret array === next bigest", function () {
+    var test3BT = new BinaryTree();
+    var value = 38;
+    var testArray = [30, 18, 22, 40, 31, 34, 21, 15];
+    test3BT.init([30, 18, 22, 38, 31, 34, 21, 15, 40]);
+    test3BT.remove(value);
+    expect(test3BT.secretArray).toEqual(testArray);
+  });
+  it("should remove value from tree", function () {
+    var test4BT = new BinaryTree();
+    var value = 38;
+    test4BT.init([30, 18, 22, 38, 31, 34, 21, 15]);
+    test4BT.remove(value);
+    expect(test4BT.search(value)).toBe(-1);
+  });
+});
+describe("BinaryTree minNode", function () {
+  var bTree = new BinaryTree();
+  it("should be defined ", function () {
+    expect(bTree.minNode).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof bTree.minNode).toBe("function");
+  });
+  it("should return -1 if don't initialize tree", function () {
+    var bTreeTest1 = new BinaryTree();
+    var privateRunMock = jest.fn();
+    bTreeTest1.privateRun = privateRunMock;
+    expect(bTreeTest1.minNode()).toBe(-1);
+    expect(bTreeTest1.privateRun).toHaveBeenCalledWith(bTreeTest1.root, 3);
+  });
+  it("should return min value and set searchValue to null", function () {
+    var b = new BinaryTree();
+    var expectResult = {
+      left: null,
+      right: { left: null, right: null, value: 15 },
+      value: -100,
+    };
+    b.init([30, 18, 22, 38, -100, 31, 34, 21, 15, 40]);
+    expect(b.minNode()).toEqual(expectResult);
+    expect(b.searchValue).toBe(null);
+  });
+});
+describe("BinaryTree maxNode", function () {
+  var bTree = new BinaryTree();
+  it("should be defined ", function () {
+    expect(bTree.maxNode).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof bTree.maxNode).toBe("function");
+  });
+  it("should return -1 if don't initialize tree", function () {
+    var bTreeTest1 = new BinaryTree();
+    var privateRunMock = jest.fn();
+    bTreeTest1.privateRun = privateRunMock;
+    expect(bTreeTest1.maxNode()).toBe(-1);
+    expect(bTreeTest1.privateRun).toHaveBeenCalledWith(bTreeTest1.root, 4);
+  });
+  it("should return max value and set searchValue to null", function () {
+    var b = new BinaryTree();
+    var expectResult = {
+      left: null,
+      right: null,
+      value: 40,
+    };
+    b.init([30, 18, 22, 38, -100, 31, 34, 21, 15, 40]);
+    expect(b.maxNode()).toEqual(expectResult);
+    expect(b.searchValue).toBe(null);
+  });
+});
+describe("BinaryTree leaves", function () {
+  var bTree = new BinaryTree();
+  it("should be defined ", function () {
+    expect(bTree.leaves).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof bTree.leaves).toBe("function");
+  });
+  it("should return 0 if we have only root in Tree", function () {
+    var test1Tree = new BinaryTree();
+    test1Tree.insert(5);
+    expect(test1Tree.leaves()).toBe(0);
+  });
+  it("should privateRun to have been called with this.root and mode === 5", function () {
+    var test2Tree = new BinaryTree();
+    test2Tree.init([3, 4, 5, 6, 7]);
+    var mockPrivateRun = jest.fn();
+    test2Tree.privateRun = mockPrivateRun;
+    test2Tree.leaves();
+    expect(test2Tree.privateRun).toHaveBeenCalledWith(test2Tree.root, 5);
+  });
+  it("should ", function () {
+    var test3Tree = new BinaryTree();
+    test3Tree.init([3, 4, 5, 6, 7]);
+    expect(test3Tree.leaves()).toBe(1);
+    expect(test3Tree.searchValue).toEqual(null);
+  });
+});
+describe("BinaryTree nodes", function () {
+  var bTree = new BinaryTree();
+  it("should be defined ", function () {
+    expect(bTree.nodes).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof bTree.nodes).toBe("function");
+  });
+  it("should return 0 if we have only root in Tree", function () {
+    var test1Tree = new BinaryTree();
+    test1Tree.insert(5);
+    expect(test1Tree.nodes()).toBe(1);
+  });
+  it("should privateRun to have been called with this.root and mode === 5", function () {
+    var test2Tree = new BinaryTree();
+    test2Tree.init([3, 4, 5, 6, 7]);
+    var mockPrivateRun = jest.fn();
+    test2Tree.privateRun = mockPrivateRun;
+    test2Tree.nodes();
+    expect(test2Tree.privateRun).toHaveBeenCalledWith(test2Tree.root, 6);
+  });
+  it("should ", function () {
+    var test3Tree = new BinaryTree();
+    test3Tree.init([3, 4, 5, 6, 7]);
+    expect(test3Tree.nodes()).toBe(4);
+    expect(test3Tree.searchValue).toEqual(null);
+  });
+});
+describe("BinaryTree size", function () {
+  var bTree = new BinaryTree();
+  it("should be defined ", function () {
+    expect(bTree.size).toBeDefined();
+  });
+  it("should be function", function () {
+    expect(typeof bTree.size).toBe("function");
+  });
+  it("should return 0 if we not insert or initialize something in Tree", function () {
+    var test1 = new BinaryTree();
+    expect(test1.size()).toBe(0);
+  });
+  it("should return 1 if we have only root in tree", function () {
+    var test2 = new BinaryTree();
+    test2.insert(1);
+    expect(test2.size()).toBe(1);
+    expect(test2.root).not.toEqual(null);
+  });
+  it("should return 1 if we have only root in tree", function () {
+    var test3 = new BinaryTree();
+    test3.init([40, 15, 31, 55, 42, 41, 61, 21, 56, 11, 28, 67, 2, 5, 39]);
+    expect(test3.size()).toBe(15);
+    expect(test3.size()).toBe(test3.secretArray.length);
   });
 });
--- HomeWorkDataStructure/DataStructure/Trees/BinaryTree.js
+++ HomeWorkDataStructure/DataStructure/Trees/BinaryTree.js
@@ -31,6 +31,7 @@ function BinaryTree() {
   };
   this.array = [];
   this.searchValue = null;
+  this.secretArray = [];
   this.privateRun = function (node, mode, value) {
     if (node) {
       if (mode === 1) {
@@ -39,18 +40,40 @@ function BinaryTree() {
         this.privateRun(node.right, 1);
       }
       if (mode === 2) {
-        if(node.value === value){
+        if (node.value === value) {
           this.searchValue = node;
           return true;
         }
-        if(node.value > value){
+        if (node.value > value) {
           this.privateRun(node.left, 2, value);
         }
-        if(node.value < value){
+        if (node.value < value) {
           this.privateRun(node.right, 2, value);
         }
-        
       }
+      if (mode === 3) {
+        this.searchValue = node;
+        this.privateRun(node.left, 3);
+      }
+      if (mode === 4) {
+        this.searchValue = node;
+        this.privateRun(node.right, 4);
+      }
+      if(mode === 5){
+        if(node.left === null && node.right === null){
+          this.searchValue++;
+        }
+        this.privateRun(node.left, 5);
+        this.privateRun(node.right, 5);
+      }
+      if(mode === 6){
+        if(node.left !== null || node.right !== null){
+          this.searchValue++;
+        }
+        this.privateRun(node.left, 6);
+        this.privateRun(node.right, 6);
+      }
+      
     }
     return -1;
   };
@@ -76,6 +99,7 @@ BinaryTree.prototype.insert = function (value) {
   } else {
     this.insertNode(this.root, newNode);
   }
+  this.secretArray.push(value);
 };
 BinaryTree.prototype.print = function (type, callback) {
   switch (type) {
@@ -97,14 +121,96 @@ BinaryTree.prototype.toArray = function () {
   return array;
 };
 BinaryTree.prototype.search = function (value) {
-  this.privateRun(this.root,2, value)
-  if(this.searchValue === null){
+  this.privateRun(this.root, 2, value);
+  if (this.searchValue === null) {
     return -1;
   }
   var result = Object.assign(this.searchValue);
   this.searchValue = null;
   return result;
 };
+
+BinaryTree.prototype.remove = function (value) {
+  var answer = this.search(value);
+  if (answer === -1) {
+    return answer;
+  } else if (answer.left && answer.right) {
+    var newArray = this.toArray();
+    var indexofValue = newArray.indexOf(value);
+    var newValue = newArray[indexofValue + 1];
+    var newArr = [];
+    for (var j = 0; j < this.secretArray.length; j++) {
+      if (this.secretArray[j] === value) {
+        this.secretArray[j] = newValue;
+        newArr.push(this.secretArray[j]);
+        continue;
+      }
+      if (this.secretArray[j] === newValue) {
+        continue;
+      }
+      newArr.push(this.secretArray[j]);
+    }
+    this.secretArray.length = 0;
+  } else {
+    var newArr = [];
+    for (var i = 0; i < this.secretArray.length; i++) {
+      if (this.secretArray[i] === value) continue;
+      newArr.push(this.secretArray[i]);
+    }
+    this.secretArray.length = 0;
+  }
+  this.root = null;
+  this.init(newArr);
+};
+BinaryTree.prototype.minNode = function () {
+  this.privateRun(this.root, 3);
+  if (this.searchValue === null) {
+    return -1;
+  }
+  var result = Object.assign(this.searchValue);
+  this.searchValue = null;
+  return result;
+};
+BinaryTree.prototype.maxNode = function () {
+  this.privateRun(this.root, 4);
+  if (this.searchValue === null) {
+    return -1;
+  }
+  var result = Object.assign(this.searchValue);
+  this.searchValue = null;
+  return result;
+};
+BinaryTree.prototype.leaves = function () {
+  if(this.root && this.root.left === null && this.root.right === null){
+    return 0;
+  }
+  this.privateRun(this.root, 5);
+  var result = this.searchValue;
+  this.searchValue = null;
+  return result;
+};
+BinaryTree.prototype.nodes = function () {
+  if(this.root && this.root.left === null && this.root.right === null){
+    return 1;
+  }
+  this.privateRun(this.root, 6);
+  var result = this.searchValue;
+  this.searchValue = null;
+  return result;
+};
+BinaryTree.prototype.size = function(){
+  if(this.root === null){
+    return 0;
+  }
+  if(this.root && this.root.left === null && this.root.right === null){
+    return 1;
+  }
+  return this.nodes() + this.leaves();
+}
+
+/* var b = new BinaryTree(); 
+b.init([40]);
+console.log(b.nodes()); */
 
 
 module.exports = BinaryTree;
